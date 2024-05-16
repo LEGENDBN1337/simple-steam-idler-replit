a1978b1090012a339af4254f5be1f6c4d3ee2c5d
Mexen1337 <algoritmartem@gmail.com>
2024-05-16 19:12:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
+var games = [730, 578080];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
 user = new steamUser();
-user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
+user.logOn({"i26xawoswwsfjz1": username, "XS4KLMGSTSJF216": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
 user.on('loggedOn', () => {
 	if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
 	user.setPersona(status);               
